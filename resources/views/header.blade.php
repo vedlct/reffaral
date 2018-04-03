75de618e720ab385d93c00fcbfc36d8086157fef
rumi <mujtaba.rumi1@gmail.com>
2018-04-03 12:50:05 +0600

    rumi

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{url('public/assets/img/apple-icon.png')}}">
+    <link rel="icon" type="image/png" href="{{url('public/assets/img/favicon.png')}}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>LLoyd Baker | Admin Panel</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
@@ -12,13 +12,13 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <!-- CSS Files -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="assets/css/now-ui-dashboard.css?v=1.0.1" rel="stylesheet" />
+    <link href="{{url('public/assets/css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="{{url('public/assets/css/now-ui-dashboard.css?v=1.0.1')}}" rel="stylesheet" />
     <!--    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css">-->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
 
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="assets/demo/demo.css" rel="stylesheet" />
+    <link href="{{url('public/assets/demo/demo.css')}}" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <style>
         .table thead tr th{
