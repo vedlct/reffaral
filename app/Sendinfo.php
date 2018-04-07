0167b2484c12c8cbc5bbd2c7f65214096b1e0720
rumi <mujtaba.rumi1@gmail.com>
2018-04-07 15:15:03 +0600

    rumi

---
+++ app/Sendinfo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sendinfo extends Model
+{
+    public $table = 'sendinfo';
+    public $timestamps = false;
+    public $primaryKey = 'sendinfoid';
+}
