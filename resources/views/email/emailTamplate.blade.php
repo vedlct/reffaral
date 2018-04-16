72bab00aaebe7b4716883699cccc981554ebb1d4
rumi <mujtaba.rumi1@gmail.com>
2018-04-16 17:19:51 +0600

    rumi

--- resources/views/email/emailTamplate.blade.php
+++ resources/views/email/emailTamplate.blade.php
@@ -326,6 +326,13 @@
                                                                                         <td height='25'></td>
                                                                                     </tr>
                                                                                     <tr>
+                                                                                        <td>{{$dicountCode}}</td>
+                                                                                    </tr>
+                                                                                    <tr>
+                                                                                        <td>{{$discountStartDate}}</td>
+                                                                                        <td>{{$discountEndDate}}</td>
+                                                                                    </tr>
+                                                                                    <tr>
                                                                                         <td>
                                                                                             <div class='contentEditableContainer contentTextEditable'>
                                                                                                 <div class='contentEditable' style='text-align: center;'>
